0fbb6b4bccc0621eb447645d02fd82194d96e9ef
xiaomai <949804781@qq.com>
2018-08-23 14:32:18 +0800

    [release][v1.1] update minSdkVersion from 9 to 7, update recyclerview to listview

--- environmentswitcher/src/main/java/com/xiaomai/environmentswitcher/EnvironmentSwitchActivity.java
+++ environmentswitcher/src/main/java/com/xiaomai/environmentswitcher/EnvironmentSwitchActivity.java
@@ -1,17 +1,16 @@
 package com.xiaomai.environmentswitcher;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.xiaomai.environmentswitcher.bean.EnvironmentConfigBean;
@@ -21,7 +20,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EnvironmentSwitchActivity extends AppCompatActivity {
+public class EnvironmentSwitchActivity extends Activity {
 
     private static final int TYPE_MODULE = 0;
 
@@ -36,7 +35,7 @@ public class EnvironmentSwitchActivity extends AppCompatActivity {
     private Adapter adapter;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.environment_switcher_activity);
         findViewById(R.id.bt_back).setOnClickListener(new View.OnClickListener() {
@@ -68,8 +67,7 @@ public class EnvironmentSwitchActivity extends AppCompatActivity {
                 String environment = (String) getXXEnvironmentMethod.invoke(environmentSwitcherClass.newInstance(), this, true);
                 environmentBean.setChecked(environment.equals(environmentBean.getUrl()));
             }
-            RecyclerView recyclerView = findViewById(R.id.recycler_view);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            ListView recyclerView = findViewById(R.id.list_view);
             adapter = new Adapter();
             recyclerView.setAdapter(adapter);
         } catch (ClassNotFoundException e) {
@@ -85,36 +83,45 @@ public class EnvironmentSwitchActivity extends AppCompatActivity {
         }
     }
 
-    class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-        @NonNull
+    class Adapter extends BaseAdapter {
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            if (getItemViewType(i) == TYPE_MODULE) {
-                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.environment_switcher_item_module, viewGroup, false);
-                return new ModuleHolder(view);
-            } else {
-                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.environment_switcher_item_environment, viewGroup, false);
-                return new EnvironmentHolder(view);
-            }
+        public int getCount() {
+            return environmentBeans.size();
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-            int itemViewType = getItemViewType(viewHolder.getAdapterPosition());
-            final EnvironmentConfigBean.ModuleBean.EnvironmentBean environmentBean = getItem(viewHolder.getAdapterPosition());
+        public EnvironmentConfigBean.ModuleBean.EnvironmentBean getItem(int position) {
+            return environmentBeans.get(position);
+        }
 
-            if (itemViewType == TYPE_MODULE) {
-                ModuleHolder holder = (ModuleHolder) viewHolder;
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final EnvironmentConfigBean.ModuleBean.EnvironmentBean environmentBean = getItem(position);
+
+            if (getItemViewType(position) == TYPE_MODULE) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.environment_switcher_item_module, parent, false);
+                TextView tvName = convertView.findViewById(R.id.tv_name);
+
                 String moduleName = environmentBean.getModuleName();
                 String alias = environmentBean.getAlias();
-                holder.tvName.setText(alias.isEmpty() ? moduleName : alias);
+                tvName.setText(TextUtils.isEmpty(alias) ? moduleName : alias);
             } else {
-                EnvironmentHolder holder = (EnvironmentHolder) viewHolder;
-                holder.tvUrl.setText(environmentBean.getUrl());
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.environment_switcher_item_environment, parent, false);
+                TextView tvName = convertView.findViewById(R.id.tv_name);
+                TextView tvUrl = convertView.findViewById(R.id.tv_url);
+                ImageView ivMark = convertView.findViewById(R.id.iv_mark);
+
+                tvUrl.setText(environmentBean.getUrl());
                 String alias = environmentBean.getAlias();
-                holder.tvName.setText(alias.isEmpty() ? environmentBean.getName() : alias);
-                holder.ivMark.setVisibility(environmentBean.isChecked() ? View.VISIBLE : View.INVISIBLE);
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                tvName.setText(TextUtils.isEmpty(alias) ? environmentBean.getName() : alias);
+                ivMark.setVisibility(environmentBean.isChecked() ? View.VISIBLE : View.INVISIBLE);
+
+                convertView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         try {
@@ -141,46 +148,17 @@ public class EnvironmentSwitchActivity extends AppCompatActivity {
                     }
                 });
             }
-        }
 
-        @Override
-        public int getItemCount() {
-            return environmentBeans.size();
+            return convertView;
         }
 
         @Override
         public int getItemViewType(int position) {
-            if (getItem(position).getName().isEmpty()) {
+            if (TextUtils.isEmpty(getItem(position).getName())) {
                 return TYPE_MODULE;
             } else {
                 return TYPE_ENVIRONMENT;
             }
-        }
-
-        EnvironmentConfigBean.ModuleBean.EnvironmentBean getItem(int position) {
-            return environmentBeans.get(position);
-        }
-    }
-
-    static class EnvironmentHolder extends RecyclerView.ViewHolder {
-        TextView tvName;
-        TextView tvUrl;
-        ImageView ivMark;
-
-        private EnvironmentHolder(@NonNull View itemView) {
-            super(itemView);
-            tvName = itemView.findViewById(R.id.tv_name);
-            tvUrl = itemView.findViewById(R.id.tv_url);
-            ivMark = itemView.findViewById(R.id.iv_mark);
-        }
-    }
-
-    static class ModuleHolder extends RecyclerView.ViewHolder {
-        TextView tvName;
-
-        private ModuleHolder(@NonNull View itemView) {
-            super(itemView);
-            tvName = itemView.findViewById(R.id.tv_name);
         }
     }
 }
